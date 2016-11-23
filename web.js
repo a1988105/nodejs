fef550fdc1808687d1aee22a20ee2d11a5bbffa7
a1988105 <a1988105@hotmail.com>
2016-11-23 22:40:27 +0800

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
-var express = require('express');
+//heroku-deploy-demo.js
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
 
-var app = express.createServer(express.logger());
+app.use(logfmt.requestLogger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
 
-var port = process.env.PORT || 3000;
+var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
