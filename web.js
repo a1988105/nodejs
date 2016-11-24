0333930fce28effe2f3e134becb75a5ede52688a
a1988105 <a1988105@hotmail.com>
2016-11-24 22:15:05 +0800

    Update web.js

--- web.js
+++ web.js
@@ -7,23 +7,4 @@ server.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
 */
-//var cool = require('cool-ascii-faces');
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index')
-});
-
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+console.log('hi~');
