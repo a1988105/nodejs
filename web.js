fc44871248afa0b20100693a1043799dc2ccb0fd
a1988105 <a1988105@hotmail.com>
2016-11-24 22:12:46 +0800

    Update web.js

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ server.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
 */
-var cool = require('cool-ascii-faces');
+//var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
@@ -23,9 +23,6 @@ app.get('/', function(request, response) {
   response.render('pages/index')
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
